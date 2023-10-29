5073dfde1e63a830683aeb378a4a07adc0b5791e
Jiavuddin <syed.jiavuddin99@gmail.com>
2023-10-29 18:10:27 +0530

    VR App Implemented

---
+++ VR/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    $(".navbar-nav .nav-link").on("click", function () {
+        $(".navbar-nav").find(".active").removeClass("active");
+        $(this).addClass("active");
+    });
+});
