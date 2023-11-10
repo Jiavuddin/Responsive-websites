caa3b5f7c87e71e2ba9df0d8d5a0d2b02dcd3176
Jiavuddin <syed.jiavuddin99@gmail.com>
2023-11-10 14:16:20 +0530

    Apps Deleted

--- Design/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    $(".navbar-nav .nav-link").on("click", function () {
-        $(".navbar-nav").find(".active").removeClass("active");
-        $(this).addClass("active");
-    });
-});
